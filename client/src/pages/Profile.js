2caa291f6202e29937f1aa066a2df1920774140e
Singgit Bramantha <bambadom@gmail.com>
2020-05-21 16:25:18 +0700

    nambah endpoint baru di user controller untuk dapat detail user dan membuat useeffect usestate di halaman profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
 
 export default function Profile(){
 
-    return <h1>Profile --- please edit</h1>
+    const [bulkPosts, setBulkPosts] = useState([])
+    const [postCount, setPostCount] = useState(0)
+    const [followerCount, setFollowerCount] = useState(0)
+    const [followingCount, setFollowingCount] = useState(0)
+    const [accountName, setAccountName] = useState(null)
+    const [accountImage, setAccountImage] = useState(null)
+    const [userLoading, setUserLoading] = useState(true)
+    const [postLoading, setPostLoading] = useState(true)
+    const [followerLoading, setFollowerLoading] = useState(true)
+    const [followingLoading, setFollowingLoading] = useState(true)
+
+    useEffect(() => {
+        axios({
+            method: "get",
+            url: `http://localhost:3000/users`,
+            headers: {token: localStorage.getItem("token")}
+        })
+        .then(response => {
+            setAccountName(response.name)
+            setAccountImage(response.image)
+            setUserLoading(false)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+        axios({
+            method: "get",
+            url: `http://localhost:3000/posts`,
+            headers: {token: localStorage.getItem("token")}
+        })
+        .then(response => {
+            setBulkPosts(response)
+            setPostCount(response.length)
+            setPostLoading(false)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+        axios({
+            method: "get",
+            url: `http://localhost:3000/follows`,
+            headers: {token: localStorage.getItem("token")}
+        })
+        .then(response => {
+            setFollowerCount(response.length)
+            setFollowerLoading(false)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+        axios({
+            method: "get",
+            url: `http://localhost:3000/follows/following`,
+            headers: {token: localStorage.getItem("token")}
+        })
+        .then(response => {
+            setFollowingCount(response.length)
+            setFollowingLoading(false)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+
+    if(postLoading || followerLoading || followingLoading || userLoading) {
+        return (
+            <>
+                <h1>Loading.....</h1>
+            </>
+        )
+    }
+
+
+    return (
+        <>
+            
+
+        </>
+
+    )
 }
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const UserController = require("../controllers/userController");
+const authentication = require('../middlewares/authentication')
 
 router.post("/login", UserController.loginUser);
 router.post("/register", UserController.registerUser);
+router.get("/", authentication, UserController.getDetailUser)
 
 module.exports = router;
