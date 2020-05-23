ce6767774933a7cd55c09c0ed4ca64f5e1695ba0
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-05-24 01:38:12 +0700

    to include server in git add

--- server/routes/commentRouter.js
+++ server/routes/commentRouter.js
@@ -4,7 +4,7 @@ const authentication = require('../middlewares/authentication')
 const authorization = require('../middlewares/authorization')
 
 router.post('/', authentication, CommentController.addComment)
-router.get('/', authentication, CommentController.getComment)
+router.get('/:PostId', authentication, CommentController.getComment)
 // router.get('/:id', authentication, CommentController.getCommentId)
 router.put('/:id', authentication, CommentController.editComment)
 router.delete('/:id', authentication, CommentController.deleteComment)
