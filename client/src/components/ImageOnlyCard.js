b123c0ea0374ba82c01a4153afc286204beb225c
Singgit Bramantha <bambadom@gmail.com>
2020-05-25 13:32:09 +0700

    nambah history push di card yang ada di profile dan explore agar bisa ke detail

--- client/src/components/ImageOnlyCard.js
+++ client/src/components/ImageOnlyCard.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Image } from 'react-bootstrap';
+import { useHistory, useLocation } from 'react-router-dom'
 
 function ImageOnlyCard(props) {
+    const history = useHistory();
+
+    function pindahkepostdetail(){
+        history.push(`/detailpost/${props.postData.id}`)
+        }
+
     return (
-        <Image style={{ height: '300px', width: '250px' }} src={props.postData.image_url} thumbnail />
+        <Image onClick={pindahkepostdetail} style={{ height: '300px', width: '250px' }} src={props.postData.image_url} thumbnail />
     )
 }
 
